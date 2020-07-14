fbc799bf18468e2f64888fe32218d00fd581dcc5
Jean Benedict Nugroho <jeanbenedict10@gmail.com>
2020-07-14 10:25:02 -0500

    custom server and its package to prepare for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // require dotenv here
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 
 const app = express();
@@ -8,18 +9,23 @@ const app = express();
 // Connect Database
 connectDB();
 
+// Init Middleware
 app.use(express.json({ extended: false }));
-
-app.get("/", (req, res) => {
-    res.send("API Running");
-});
 
 // Define Routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/auth", require("./routes/api/auth"));
 
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+    // Set static folder
+    app.use(express.static("client/build"));
 
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 
